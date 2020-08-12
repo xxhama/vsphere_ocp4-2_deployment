df44e439d977c44b669c8ec63cc0358d50afe9d6
Johnny Xhama <xhama@ibm.com>
2020-08-12 15:59:58 -0500

    created vm for testing

--- ign-file-server/ign-file-server.tf
+++ ign-file-server/ign-file-server.tf
@@ -10,4 +10,13 @@ resource "null_resource" "copy_ign_file" {
     source = "./installer-files/bootstrap.ign"
     destination = "/tmp/ignition"
   }
+
+  provisioner "remote-exec" {
+    inline = [
+      "systemctl stop firewalld",
+      "yum install httpd -y",
+      "systemctl start httpd",
+      "ln -s /tmp/ignition /var/www/html"
+    ]
+  }
 }
