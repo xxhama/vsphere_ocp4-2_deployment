c54461cbdf42ab6dd9cdfee509cfec0399f4bf0c
Johnny Xhama <xhama@ibm.com>
2020-08-17 19:52:45 -0500

    added ign path for ign-file-server

--- ign-file-server/variables.tf
+++ ign-file-server/variables.tf
@@ -2,10 +2,14 @@ variable "infra_host" {
   type = string
   default = "172.18.8.240"
 }
-
 variable "infra_private_key" {
   type = string
 }
+
+variable "ign_path" {
+  type = string
+}
+
 variable "dependsOn" {
   default     = "true"
   description = "Boolean for dependency"
--- ignition/output.tf
+++ ignition/output.tf
@@ -10,8 +10,8 @@ output "worker_ignition" {
   value = data.local_file.worker_ign.content
 }
 
-output "bootstrap_ignition" {
-  value = data.local_file.bootstrap_ign.content
+output "bootstrap_ignition_path" {
+  value = data.local_file.bootstrap_ign.filename
 }
 
 output "kubeadmin_password" {
