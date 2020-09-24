41fa0e0c711762f89cf3cb8f1b680f4d2c7b7d15
Isaac Zavala <isaac.zavala@ibm.com>
2020-09-24 15:39:34 -0500

    Update cluster_deployment_complete.tf

--- cluster_deployment_complete/cluster_deployment_complete.tf
+++ cluster_deployment_complete/cluster_deployment_complete.tf
@@ -20,8 +20,8 @@ resource "null_resource" "approve_csr" {
       "ssh core@${var.bootstrap_ip}",
       "until journalctl -b  -u bootkube.service |grep -o 'bootkube.service complete';do echo 'waiting for bootstrap to complete'; sleep 1m ;done",
       "exit",
-      "chmod 777 /usr/local/bin/kubectl",
-      "chmod 777 /usr/local/bin/oc",
+      "chmod +x /usr/local/bin/kubectl",
+      "chmod +x /usr/local/bin/oc",
       "export KUBECONFIG=/opt/kubeconfig",
       "for i in {1 3 5 10}; do oc get csr -o name | xargs oc adm certificate approve; sleep 1m; done",
 
